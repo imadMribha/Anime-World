4fae6ecd1331ac5d79ef66568ad3443bf972d951
imadMribha <imadmribha@gmail.com>
2025-05-07 13:52:44 +0100

    feat: implement JwtService with token generation, validation, and extraction methods

--- src/main/java/com/animeworld/animeapp/service/JwtService.java
+++ src/main/java/com/animeworld/animeapp/service/JwtService.java
@@ -1,9 +1,70 @@
 package com.animeworld.animeapp.service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
+	//Secret key for signing the token
+	@Value("${JWT_SECRET}")
+	private String secretKey;
 	
+	//Expiration time for the token
+	@Value("${JWT_EXPIRATION}")
+	private long expirationTime;
+	
+	// Generate a JWT token
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+				.signWith(getSignInKey(), SignatureAlgorithm.HS256)
+				.compact();
+	}
+	
+	//Extract the username from the token
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	//Validate the token 
+	public boolean isTokenValid(String token, String username) {
+		final String extractedUsername = extractUsername(token);
+		return extractedUsername.equals(username) && !isTokenExpired(token);
+	}
+	
+	//Helpers 
+	
+	//Extract claims from the token
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignInKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+	}
+	
+	private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		return claimsResolver.apply(extractAllClaims(token));
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractClaim(token, Claims::getExpiration).before(new Date());
+	}
+	
+	private Key getSignInKey() {
+		return Keys.hmacShaKeyFor(secretKey.getBytes());
+	}
 }
